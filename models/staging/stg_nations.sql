a40262b6ff136472079e376c09de11735304863c
Nikita Sokolov <nikita_sokolov@epam.com>
2022-07-01 20:05:29 +0200

    Try to use internal stage as source

---
+++ models/staging/stg_nations.sql
@@ -0,0 +1,7 @@
+select 
+    cast(t.$1 as number(3, 0)) NATIONKEY, 
+    trim(t.$2) NAME, 
+    cast(t.$3 as number(1,0)) REGIONKEY, 
+    trim(t.$4) COMMENT
+from @public.int_stg/h_nation.dsv.gz
+(file_format => public.dsv) t;
