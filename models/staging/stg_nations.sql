ad78457d4e363532f1ae0e0a1ecafc6507b37413
Nikita Sokolov <nikita_sokolov@epam.com>
2022-07-01 20:21:08 +0200

    ref to src

--- models/staging/stg_nations.sql
+++ models/staging/stg_nations.sql
@@ -3,5 +3,7 @@ select
     trim(t.$2) NAME, 
     cast(t.$3 as number(1,0)) REGIONKEY, 
     trim(t.$4) COMMENT
-from @public.int_stg/h_nation.dsv.gz
-(file_format => public.dsv) t;
+from {{ source('int_stg', 'nations') }}
+--@public.int_stg/h_nation.dsv.gz
+--(file_format => public.dsv) t
+;
